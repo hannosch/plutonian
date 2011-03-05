2af450e09e237bac3f1623d21e431ca12c3526cb
Hanno Schlichting <hanno@jarn.com>
2011-03-05 19:31:51 +0100

    vb

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='plutonian',
-    version = '0.1a1',
+    version = '0.1a2',
     author='Hanno Schlichting',
     author_email='hanno@hannosch.eu',
     description='Pyramid-style development for Plone.',
